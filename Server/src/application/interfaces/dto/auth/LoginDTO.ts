474f1ca020e3f4a632c636a112118279b30e443c
Ajai-3 <ajaipjayan@gmail.com>
2025-11-07 17:06:30 +0530

    feat(auth): add login and user registration DTOs
    Add data transfer objects for authentication endpoints to define request/response structures

---
+++ Server/src/application/interfaces/dto/auth/LoginDTO.ts
@@ -0,0 +1,4 @@
+export interface LoginDto {
+    idnetifire: string,
+    password: string
+}
---
+++ Server/src/application/interfaces/dto/auth/UserRegisterDTO.ts
@@ -0,0 +1,12 @@
+export interface UserRegisterDTO {
+  name: string;
+  email: string;
+  username: string;
+  password: string;
+}
+
+export interface UserResponseDTO {
+  id: string;
+  name: string;
+  username: string;
+}
