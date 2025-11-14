3553422b0f4a1803f41bef8c14cc12dd22c37e66
Ajai-3 <ajaipjayan@gmail.com>
2025-11-14 13:45:14 +0530

    feat(auth): add reset password page with form validation
    Implement password reset functionality with form validation to ensure password matching. Includes input fields for new password and confirmation, with disabled submit button until form is complete.

---
+++ Client/src/features/user/pages/auth/ResetPassword.tsx
@@ -0,0 +1,100 @@
+import { Shield } from "lucide-react";
+import React, { useState } from "react";
+import FormError from "@/components/ui/FormError";
+import Input from "../../../../components/ui/Input";
+import Button from "../../../../components/ui/Button";
+import AuthLinkLine from "@/components/ui/AuthLinkLine";
+import AuthPageWrapper from "../../components/auth/AuthPageWrapper";
+
+const ResetPassword: React.FC = () => {
+  const [passwords, setPasswords] = useState({
+    password: "",
+    confirmPassword: "",
+  });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setPasswords((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if (!passwords.password || !passwords.confirmPassword) return;
+    if (passwords.password !== passwords.confirmPassword) {
+      alert("Passwords do not match");
+      return;
+    }
+
+  };
+
+  const isFormComplete =
+    passwords.password.length > 0 && passwords.confirmPassword.length > 0;
+
+  return (
+    <AuthPageWrapper
+      title="Reset your password"
+      error=""
+      subtitle=""
+      linkText=""
+      linkHref=""
+      icon={<Shield className="w-8 h-8 text-emerald-500" />}
+    >
+      <p className="text-zinc-400 text-sm mb-6 text-center">
+        Enter your new password below
+      </p>
+      <form onSubmit={handleSubmit} className="space-y-2">
+        <div>
+          <label className="block text-sm font-medium text-zinc-200 mb-1">
+            Password
+          </label>
+          <Input
+            type="password"
+            password
+            name="password"
+            value={passwords.password}
+            onChange={handleChange}
+            placeholder="New Password"
+            autoComplete="new-password"
+            inputSize="sm"
+          />
+          <FormError message={false || ""} />
+        </div>
+
+        <div>
+          <label className="block text-sm font-medium text-zinc-200 mb-1">
+            Conform Password
+          </label>
+          <Input
+            type="password"
+            password
+            name="confirmPassword"
+            value={passwords.confirmPassword}
+            onChange={handleChange}
+            placeholder="Confirm Password"
+            autoComplete="new-password"
+            inputSize="sm"
+          />
+          <FormError message={false || ""} />
+        </div>
+
+        <Button
+          variant="auth"
+          size="sm"
+          type="submit"
+          disabled={!isFormComplete}
+        >
+          Reset Password
+        </Button>
+      </form>
+      <AuthLinkLine
+        text="Remembered your password?"
+        linkText="Sign in"
+        linkTo="/auth/login"
+        mtClass="mt-4"
+      />
+    </AuthPageWrapper>
+  );
+};
+
+export default ResetPassword;
