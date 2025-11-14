d3863ebdc4efaf06a12c4835d97ff0d1120b82cc
Ajai-3 <ajaipjayan@gmail.com>
2025-11-14 13:42:49 +0530

    refactor(auth): extract auth link and error display into components
    - Replace inline Link with AuthLinkLine component for better reusability
    - Add FormError component to display authentication errors
    - Adjust spacing and padding for better visual hierarchy

--- Client/src/features/user/components/auth/AuthPageWrapper.tsx
+++ Client/src/features/user/components/auth/AuthPageWrapper.tsx
@@ -1,43 +1,47 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import { Popcorn } from "lucide-react";
+import FormError from "@/components/ui/FormError";
+import AuthLinkLine from "@/components/ui/AuthLinkLine";
 
 interface AuthPageProps {
   title: string;
+  error: string;
   subtitle: string;
   linkText: string;
   linkHref: string;
-  children: React.ReactNode;
   icon?: React.ReactNode;
+  children: React.ReactNode;
 }
 
 const AuthPageWrapper: React.FC<AuthPageProps> = ({
   title,
+  error,
   subtitle,
   linkText,
   linkHref,
   children,
   icon = <Popcorn className="w-8 h-8 text-main-color-3" />,
 }) => {
+  console.log(error);
   return (
     <div className="w-full max-w-md">
-      <div className="text-center mb-4 sm:mb-10">
-        <div className="flex items-center justify-center mb-4">
+      <div className="text-center mb-4 sm:mb-4">
+        <div className="flex items-center justify-center mb-2">
           {icon}
           <h1 className="text-4xl font-bold text-white ml-2">FlixList</h1>
         </div>
-        <h2 className="text-3xl font-bold text-white mb-2">{title}</h2>
-        <p className="text-zinc-400 text-sm">
-          {subtitle}{" "}
-          <Link
-            to={linkHref}
-            className="font-semibold text-main-color-3 hover:text-main-color-4"
-          >
-            {linkText}
-          </Link>
-        </p>
+        <h2 className="text-3xl font-bold text-white">{title}</h2>
+        <AuthLinkLine
+          text={subtitle}
+          linkText={linkText}
+          linkTo={linkHref}
+          mtClass="mt-1"
+        />
       </div>
-      <div className="p-8">{children}</div>
+
+      <FormError className="text-center" message={error} size="lg" />
+
+      <div className="px-8 py-4">{children}</div>
     </div>
   );
 };
